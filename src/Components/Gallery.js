f058c1fb2acbf462abe68e48a6b622deeced49ba
César Tapia <cesar1tapia1@gmail.com>
2021-02-09 23:36:20 -0300

    0.0.2

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -5,8 +5,16 @@ import useFetchImages from '../hooks/useFetchImages';
 export const Gallery = ({search}) => {
     const {results:images, loading} = useFetchImages( search );
 
- 
+    const index = images.length;
+    
+    const divTres =  images.splice(index*2/3, index);
+
+    const divDos = images.splice(index/3, (index*2/3));
+    const divUno = images.splice(0,index/3);
+    
    
+    console.log(index)
+
     return(
         <>
             <h3>{ search}</h3>
@@ -14,7 +22,30 @@ export const Gallery = ({search}) => {
 
             <div className="grid-gallery">
                 {
-                    images.map( img => (
+                    divUno.map( img => (
+                        
+                        <ImagesItem 
+                            key={ img.id}
+                            {...img}
+                        />
+                    ))
+                }
+            </div>
+            <div className="grid-gallery">
+                {
+                    divDos.map( img => (
+                        
+                        <ImagesItem 
+                            key={ img.id}
+                            {...img}
+                        />
+                    ))
+                }
+            </div>
+            <div className="grid-gallery">
+                {
+                    divTres.map( img => (
+                        
                         <ImagesItem 
                             key={ img.id}
                             {...img}
